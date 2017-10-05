35d824466cd9668ada0e672867caa6d729efbfcc
farrah <marzini.f@gmail.com>
2017-10-05 17:49:59 +0200

    started the visible part for details.php

--- vue/espace_admin/details.php
+++ vue/espace_admin/details.php
@@ -4,9 +4,9 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/moreauandsons/vue/includes/header.php')
   </nav>
 </header>
 
-<main class="">
+<main class="details_main">
   <!-- ICI mettre le details d'un etape et la possibilité de modifier/ajouter/supprimer/valider -->
-  <div id="details" class="row">
+  <div id="details" class="row" style="width : 90%; padding-top : 10px;">
     <div class="projet card sticky-action col s12 m6 l3">
       <div class="card-image waves-effect waves-block waves-light">
         <?php
@@ -48,47 +48,66 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/moreauandsons/vue/includes/header.php')
         <?php  }  ?>
       </div>
       <div class="card-content">
-        <span class="card-title activator teal-text text-darken-1"><?php echo $ONEproject['nom_projet'];?><i class="fa fa-plus-circle teal-text fa-2x text-darken-1" aria-hidden="true"></i></span>
+        <span class="card-title activator teal-text text-darken-1"><?php echo $ONEproject['nom_projet'];?></span>
         <p>Démarrage : <?php echo $ONEproject['date_creation']?> </p>
         <p>Remise des clés : <?php echo $ONEproject['delai']?></p>
         <p>Client : <?php echo $ONEproject['nom_client']?> </p>
         <p>Ville : <?php echo $ONEproject['ville']?> </p>
         <p>Budget : <?php echo $ONEproject['budget']?>€ </p>
       </div>
-      <div class="card-reveal">
-        <span class="card-title teal-text text-darken-1">Liste étapes<i class="material-icons right">close</i></span>
-        <p>Cliquez pour un descriptif.</p>
-        <table class="highlight row">
-          <thead class="white-text teal darken-1">
-            <tr>
-                <th>Intitulé</th>
-                <th>Délai</th>
-                <th>Etat</th>
-            </tr>
-          </thead>
-
-          <tbody class="">
-            <tr>
-              <td><a href="http://localhost/moreauandsons/controler/espace_admin/details.php">Nom de l'étape</a></td>
-              <td>DD/MM/YY</td>
-              <td class="state_step red"></td>
-            </tr>
-            <tr>
-              <td><a href="#">Nom de l'étape</a></td>
-              <td>DD/MM/YY</td>
-              <td class="state_step red"></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
       <div class="card-action row">
-        <a title="supprimer le projet, attention action irréversible" class="col s1" href="#"> <i class="fa fa-trash teal-text" aria-hidden="true"></i></a>
-        <a title="archiver le projet, utile lorsqu'il est terminé" class="col s1" href="#"><i class="fa fa-folder-open teal-text" aria-hidden="true"></i></a>
-        <a class="teal-text" href="http://localhost/moreauandsons/controler/espace_admin/details.php?id_projet=<?php echo $projet['ID'];?>">ajout étape</a>
+          <a class="col s12 btn teal darken-1" style="position : relative; bottom : 10px;" href="http://localhost/moreauandsons/controler/espace_admin/espace_admin.php">Retour</a>
       </div>
     </div>
-    <a class="btn teal darken-1" href="http://localhost/moreauandsons/controler/espace_admin/espace_admin.php">Retour</a>
+
+
+    <!-- partie collapsible  -->
+    <ul class="collapsible col s12 m6 l8 offset-l1" style="border: 0px;" data-collapsible="accordion">
+      <li>
+        <div style="display: flexbox; flex-flow : row wrap; justify-content : space-around;" class="collapsible-header">
+          <span>Titre etape</span>
+          <span>DD/MM/YYYY</span>
+          <span style="background-color: red; width : 25px;"></span>
+        </div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div style="display: flexbox; flex-flow : row wrap; justify-content : space-around;" class="collapsible-header">
+          <span>Titre etape</span>
+          <span>DD/MM/YYYY</span>
+          <span style="background-color: red; width : 25px;"></span>
+        </div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div style="display: flexbox; flex-flow : row wrap; justify-content : space-around;" class="collapsible-header">
+          <span>Titre etape</span>
+          <span>DD/MM/YYYY</span>
+          <span style="background-color: red; width : 25px;"></span>
+        </div>
+        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      </li>
+      <li>
+        <div style="display: flexbox; flex-flow : row wrap; justify-content : space-around;">
+          <form class="" action="index.html" method="post">
+            <div class="input-field col s6">
+              <input id="name_step" type="text" class="validate" required>
+              <label for="name_step">Intitulé de l'étape</label>
+            </div>
+            <div class="input-field col s6">
+              <input id="date_step" type="text" class="validate" required>
+              <label for="date_step">Délai prévu <em>(format DD/MM/YYYY)</em></label>
+            </div>
+            <input class="btn teal darken-1 right" style="margin : 10px;" type="submit" name="" value="Ajouter étape">
+          </form>
+        </div>
+      </li>
+    </ul>
+
   </div>
+
+
+
 
 </main>
 
